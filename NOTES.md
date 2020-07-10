f9d88afc6d88d2a887fbac7f16804fe396db074b
Peter Siska <psiska@inventage.com>
2020-07-10 15:10:48 +0200

    Add basic MWC layout

--- NOTES.md
+++ NOTES.md
@@ -5,6 +5,8 @@
     - Install [Prettier plugin](https://plugins.jetbrains.com/plugin/10456-prettier/)
     - Format on save: `Prettier` → `Run on save for files`
     - Increase width with `printWidth: 160` (add to `package.json`)
+- Remove `amenity-finder.js`, use `customElements.define('amenity-finder', AmenityFinder);` in `AmenityFinder.js` directly
+    - The separation only makes sense if we want to provide JS Classes + Custom Elements separately
 - Add basic layout with Material WC:
     - Install components `$ npm install @material/mwc-top-app-bar-fixed @material/mwc-icon @material/mwc-icon-button @material/mwc-drawer`
     - Add fonts
@@ -13,3 +15,4 @@
           <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
           <link href="https://fonts.googleapis.com/css?family=Material+Icons&display=block" rel="stylesheet">
       ```
+    - Add sidebar property, toggle method + `MDCDrawer:closed` listener! 
--- src/AmenityFinder.js
+++ src/AmenityFinder.js
@@ -1,74 +1,72 @@
 import { LitElement, html, css } from 'lit-element';
-import { openWcLogo } from './open-wc-logo.js';
+
+import '@material/mwc-drawer';
+import '@material/mwc-icon';
+import '@material/mwc-icon-button';
+import '@material/mwc-top-app-bar-fixed';
 
 export class AmenityFinder extends LitElement {
   static get properties() {
     return {
-      title: { type: String },
-      page: { type: String },
+      showSidebar: { type: Boolean },
     };
   }
 
   static get styles() {
     return css`
-      :host {
-        min-height: 100vh;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: flex-start;
-        font-size: calc(10px + 2vmin);
-        color: #1a2b42;
-        max-width: 960px;
-        margin: 0 auto;
-        text-align: center;
-      }
-
-      main {
-        flex-grow: 1;
-      }
-
-      .logo > svg {
-        margin-top: 36px;
-        animation: app-logo-spin infinite 20s linear;
-      }
-
-      @keyframes app-logo-spin {
-        from {
-          transform: rotate(0deg);
-        }
-        to {
-          transform: rotate(360deg);
-        }
-      }
-
-      .app-footer {
-        font-size: calc(12px + 0.5vmin);
-        align-items: center;
-      }
-
-      .app-footer a {
-        margin-left: 5px;
+      .container {
+        padding: 1rem;
       }
     `;
+  }
+
+  constructor() {
+    super();
+
+    this.showSidebar = false;
+
+    this.addEventListener('MDCDrawer:closed', () => {
+      this.showSidebar = false;
+    });
   }
 
   render() {
     return html`
-      <main>
-        <div class="logo">${openWcLogo}</div>
-        <h1>My app</h1>
-
-        <p>Edit <code>src/AmenityFinder.js</code> and save to reload.</p>
-        <a class="app-link" href="https://open-wc.org/developing/#code-examples" target="_blank" rel="noopener noreferrer">
-          Code examples
-        </a>
-      </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a target="_blank" rel="noopener noreferrer" href="https://github.com/open-wc">open-wc</a>.
-      </p>
+      <mwc-drawer
+        hasHeader
+        type="modal"
+        .open="${this.showSidebar}"
+        @MDCDrawer:closed="${() => {
+          this.showSidebar = false;
+        }}"
+      >
+        <span slot="title">Menu</span>
+        <div class="container">
+          <p>Drawer content!</p>
+        </div>
+        <div slot="appContent">
+          <mwc-top-app-bar-fixed>
+            <mwc-icon-button icon="menu" slot="navigationIcon" @click="${this._toggleSidebar}"></mwc-icon-button>
+            <div slot="title">Amenity Finder</div>
+          </mwc-top-app-bar-fixed>
+          <main class="container">
+            <h1>Hello, stranger!</h1>
+            <p>
+              Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in
+              Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with
+              the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing
+              has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum
+              decided to
+            </p>
+          </main>
+        </div>
+      </mwc-drawer>
     `;
   }
+
+  _toggleSidebar() {
+    this.showSidebar = !this.showSidebar;
+  }
 }
+
+customElements.define('amenity-finder', AmenityFinder);
